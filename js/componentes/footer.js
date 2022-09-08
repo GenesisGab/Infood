f65d8110962b5a78a6a6704256585dd464cfe057
ndmarroquin <ndmarroquin.scl.edu.gt>
2022-09-08 17:57:59 -0600

    Parte de Natali completada

--- js/componentes/footer.js
+++ js/componentes/footer.js
@@ -5,7 +5,24 @@ export function footer (){
     
     /*PRIMERA SECCIÓN INTEGRANTES */
     '<section class="integrantes">'+
+    '<div class="caja1">'+
+        '<h1>Comunicadores graficos</h1>'+
+        '<p>Adriana Alejandra Velásquez Anleú</p>'+
+        '<p>Marilis Abigail Escobar Estrada</p>'+
+    '</div>'+
     
+    '<div class="caja2">'+
+        '<h1>Biólogos</h1>'+
+        '<p>Joseline Johanna Quisque Rustrián</p>'+
+        '<p>María de los Angeles Del Cid García</p>'+
+    '</div>'+
+    
+    '<div class="caja3">'+
+        '<h1>Desarrolladores</h1>'+
+        '<p>Kimberly Estephania Hernández Ortiz</p>'+
+        '<p>Natali Daniela Marroqin Garrido</p>'+
+        '<p>Génesis Gabriel Subuyuj Ramírez</p>'+
+    '</div>'+
     '</section>'+
     
     /*SEGUNDA SECCION INFORMACION EXTRA */
