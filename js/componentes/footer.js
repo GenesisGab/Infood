464586e4b42e236486f465c4a42e14a4f78c622c
kimmberly439 <kimorhe123gmil.com>
2022-09-08 20:19:09 -0600

    se termino la partre de la informacion eztra del fooder

--- js/componentes/footer.js
+++ js/componentes/footer.js
@@ -11,6 +11,26 @@ export function footer (){
     /*SEGUNDA SECCION INFORMACION EXTRA */
     '<section class="infoInfood">'+
 
+    '<div class="ventana" id="vent">'+
+'<h1 class="ventana_titulo">INFOOD</h1></div>'+
+
+    '<div class="padreFooter">'+
+        '<div class="ventana_fondo">'+
+        '<img  class="ventana_img" src="./img/alimentos.png">'+
+            '<h1 class="ventana_titulo5">LOREM</h1>'+
+            '<p class="ventana_parrafo">Lorem lpsum is simply dummy text of the printing and typesetting industry. Lorem lpsum'+
+                'has been the industrys standard  dummy text ever since  the 1500s, when an un unknown printer'+
+                'took'+
+            '</p>'+
+        '</div>'+
+
+        '<div class="ventana_titulo3">'+
+            '<h2>Lorem lpsum</h2>'+
+            '<p>lorem ipsu8m is simply dummy text of the printing and typesetting industry. Lorem lpsum has been the industrys strandard dummy txtever since the 150s. when an unknownprinter took. Lorem ipsum is simply dummy text of the printing and typesetting industry , Lorem lpsum'+
+        'has been the industrys standard dummy text ever since the 1500s, when on unknown pinter look Lorem lpsum is simply dummy text of the printing abd typesetting industry . Lorem lpsum has been the industrys standard dummy text ever since the 1500s, when an unknawn printer took. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>'+
+        '</div>'+
+    '</div>'+
+
     '</section>';
 }
 footer();
