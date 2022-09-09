267d044cce36ae3f576a50948bb170973b8741bf
ndmarroquin <ndmarroquin.scl.edu.gt>
2022-09-08 22:11:26 -0600

    Sección de los integrantes para telefono terminada

--- js/componentes/footer.js
+++ js/componentes/footer.js
@@ -4,25 +4,26 @@ export function footer (){
     root.innerHTML = 
     
     /*PRIMERA SECCIÓN INTEGRANTES */
-    '<section class="integrantes">'+
-    '<div class="caja1">'+
-        '<h1>Comunicadores graficos</h1>'+
-        '<p>Adriana Alejandra Velásquez Anleú</p>'+
-        '<p>Marilis Abigail Escobar Estrada</p>'+
-    '</div>'+
     
-    '<div class="caja2">'+
-        '<h1>Biólogos</h1>'+
-        '<p>Joseline Johanna Quisque Rustrián</p>'+
-        '<p>María de los Angeles Del Cid García</p>'+
-    '</div>'+
-    
-    '<div class="caja3">'+
-        '<h1>Desarrolladores</h1>'+
-        '<p>Kimberly Estephania Hernández Ortiz</p>'+
-        '<p>Natali Daniela Marroqin Garrido</p>'+
-        '<p>Génesis Gabriel Subuyuj Ramírez</p>'+
-    '</div>'+
+    '<section class="integrantes" data-aos="flip-left">'+
+       '<div class="caja1">'+
+            '<h1>Comunicadores graficos</h1>'+
+            '<p>Adriana Alejandra <br> Velásquez Anleú</p>'+
+            '<p>Marilis Abigail <br> Escobar Estrada</p>'+
+        '</div>'+
+        
+        '<div class="caja1" id="biologos">'+
+            '<h1>Biólogos</h1>'+
+            '<p>Joseline Johanna <br> Quisque Rustrián</p>'+
+            '<p>María de los Angeles <br> Del Cid García</p>'+
+        '</div>'+
+        
+        '<div class="caja1">'+
+            '<h1>Desarrolladores</h1>'+
+            '<p>Kimberly Estephania <br> Hernández Ortiz</p>'+
+            '<p>Natali Daniela <br> Marroqin Garrido</p>'+
+            '<p>Génesis Gabriel <br> Subuyuj Ramírez</p>'+
+        '</div>'+
     '</section>'+
     
     /*SEGUNDA SECCION INFORMACION EXTRA */
