332eae4f0c4449ce8762ddd45c6053629e575f91
kimberly Hernandez <79287975+kimmberly439@users.noreply.github.com>
2022-09-19 19:01:55 -0600

    se coloca la base de datos de proteínas

---
+++ js/data/objetosProteinas.js
@@ -0,0 +1,178 @@
+export let proteinass = [
+    {
+        titulo: 'Pollo, pechuga cruda',
+        img: '../img/pollo.png',
+        porcion: '100g',
+        Energía: '195Kcal',
+        calorias: '172cal',
+        Proteinas: '20.85g',
+        carbohidratos: '0',
+        lipidos: '2.1%',
+    },
+    {
+        titulo: 'Huevo de gallina',
+        img: '../img/huevo.png',
+        porcion: '63g',
+        Energía: '52Kcal',
+        calorias: '345cal',
+        Proteinas: '20.5g',
+        carbohidratos: '2.79g',
+        lipidos: '2.9%',
+    },
+    
+    {
+        titulo: 'Leche de vaca',
+        img: '../img/leche.png',
+        porcion: '250ml',
+        Energía: '122Kcal',
+        calorias: '358cal',
+        Proteinas: '35.10g',
+        carbohidratos: '52.19g',
+        lipidos: '1.8%',
+    },
+    
+    {
+        titulo: 'Res, carne magra cruda',
+        img: '../img/carnes.png',
+        porcion: '100g',
+        Energía: '126Kcal',
+        calorias: '126cal',
+        Proteinas: '22.03g',
+        carbohidratos: '0',
+        lipidos: '59%',
+    },
+    
+    {
+        titulo: 'Jamon de pavo',
+        img: '../img/jamon.png',
+        porcion: '100g',
+        Energía: '30Kcal',
+        calorias: '147cal',
+        Proteinas: '18.70g',
+        carbohidratos: '0.53g',
+        lipidos: '0.9%',
+    },
+    
+    {
+        titulo: 'Queso blanco',
+        img: '../img/queso.png',
+        porcion: '15ml',
+        Energía: '60Kcal',
+        calorias: '173cal',
+        Proteinas: '24.35g',
+        carbohidratos: '1.91g',
+        lipidos: '21%',
+    },
+    
+    {
+        titulo: 'Atun enlatado ',
+        img: '../img/atun.png',
+        porcion: '100g',
+        Energía: '108Kcal',
+        calorias: '186cal',
+        Proteinas: '186g',
+        carbohidratos: '0',
+        lipidos: '8%',
+    },
+
+    {
+        titulo: 'Pescado carne, salmon',
+        img: '../img/salmon.png',
+        porcion: '100g',
+        Energía: '84Kcal',
+        calorias: '179cal',
+        Proteinas: '19.93g',
+        carbohidratos: '0',
+        lipidos: '12%',
+    },
+
+    {
+        titulo: 'Arroz blanco',
+        img: '../img/arroz.png',
+        porcion: '250ml',
+        Energía: '191Kcal',
+        calorias: '374cal',
+        Proteinas: '20.85g',
+        carbohidratos: '66.17g',
+        lipidos: '0.6%',
+    },
+
+    {
+        titulo: 'Frijol blanco, grano seco',
+        img: '../img/frijolBlanco.png',
+        porcion: '38.5g',
+        Energía: '139Kcal',
+        calorias: '333cal',
+        Proteinas: '23.36g',
+        carbohidratos: '60.27g',
+        lipidos: '0',
+    },
+
+    {
+        titulo: 'Frijol negro, grano seco',
+        img: '../img/frijolNegro.png',
+        porcion: '38.5g',
+        Energía: '218Kcal',
+        calorias: '343cal',
+        Proteinas: '22.7g',
+        carbohidratos: '61.6g',
+        lipidos: '0',
+    },
+
+    {
+        titulo: 'Arveja',
+        img: '../img/Arveja.png',
+        porcion: '82g',
+        Energía: '42Kcal',
+        calorias: '341cal',
+        Proteinas: '24.55g',
+        carbohidratos: '60.37g',
+        lipidos: '0.2%',
+    },
+
+    {
+        titulo: 'Almendra seca',
+        img: '../img/almendraSeca.png',
+        porcion: '100g',
+        Energía: '7Kcal',
+        calorias: '581cal',
+        Proteinas: '21.94g',
+        carbohidratos: '19.94g',
+        lipidos: '9%',
+    },
+
+
+    {
+        titulo: 'Aguacate',
+        img: '../img/Aguacate.png',
+        porcion: '200g',
+        Energía: '322Kcal',
+        calorias: '160cal',
+        Proteinas: '2g',
+        carbohidratos: '8.53g',
+        lipidos: '3.2%',
+    },
+
+    {
+        titulo: 'Brocoli',
+        img: '../img/Brocoli.png',
+        porcion: '500g',
+        Energía: '31Kcal',
+        calorias: '34cal',
+        Proteinas: '2.82g',
+        carbohidratos: '6.64g',
+        lipidos: '0.1%',
+    },
+
+    {
+        titulo: 'Nuez seca',
+        img: '../img/nuez.png',
+        porcion: '100g',
+        Energía: '170Kcal',
+        calorias: '656cal',
+        Proteinas: '14.32g',
+        carbohidratos: '12.27g',
+        lipidos: '67.2%',
+    }
+    
+];
